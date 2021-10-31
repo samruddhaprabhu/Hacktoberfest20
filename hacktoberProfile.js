a28c117dccd0f0a0bc020044009619579757d035
samruddhaprabhu <38967050+samruddhaprabhu@users.noreply.github.com>
2021-10-31 18:48:08 +0530

    Update hacktoberProfile.js
    updated my name

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -25,6 +25,11 @@ var profiles = [
     name : "Vaishnav",
     place : "Edappal, India",
     github : "vaishnavmohan",
-  }
+  },
+  {
+    name : "Samruddha",
+    place: "Karnataka",
+    github: "samruddhaprabhu",
+  }  
 ];
 
